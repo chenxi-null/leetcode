cbe79793b491d8cfce7ef3fdc9b2c026d6ff11a4
chenxi <chenxi01@souche.com>
2020-07-24 10:24:01 +0800

    #dp 64 minimum path sum, O(n)

--- src/main/java/com/leetcode/dp/_64_MinimumPathSum.java
+++ src/main/java/com/leetcode/dp/_64_MinimumPathSum.java
@@ -17,7 +17,23 @@ Explanation: Because the path 1→3→1→1→1 minimizes the sum.
 
 https://leetcode-cn.com/problems/minimum-path-sum
 
+---
+
+Solution:
+
 #dp
+
+经典的二维 DP 问题，递推方程很容易想到:
+
+dp(i,j) = min{ dp(i-1,j), dp(i,j-1) } + cost(i,j)
+
+时间复杂度：O(mn)
+空间复杂度：O(mn)
+
+有一些减少空间复杂度的技巧:
+- O(n): 二维 dp 数组变成一维度的
+- O(1): 在给定的数组上原地 (in-place) dp
+
  */
 
 /**
@@ -29,21 +45,51 @@ public class _64_MinimumPathSum {
         if (a == null || a.length == 0 || a[0] == null || a[0].length == 0) {
             return 0;
         }
-        int m = a.length;
-        int n = a[0].length;
-        int[][] dp = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int min = Integer.MAX_VALUE;
-                if (i > 0) {
-                    min = dp[i - 1][j];
-                }
-                if (j > 0) {
-                    min = Math.min(min, dp[i][j - 1]);
-                }
-                dp[i][j] = min;
+        int nr = a.length;
+        int nc = a[0].length;
+        int[][] dp = new int[nr][nc];
+
+        dp[0][0] = a[0][0];
+        for (int i = 1; i < nr; i++) {
+            dp[i][0] = dp[i - 1][0] + a[i][0];
+        }
+        for (int j = 1; j < nc; j++) {
+            dp[0][j] = dp[0][j - 1] + a[0][j];
+        }
+
+        for (int i = 1; i < nr; i++) {
+            for (int j = 1; j < nc; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + a[i][j];
             }
         }
-        return dp[m - 1][n - 1];
+        return dp[nr - 1][nc - 1];
+    }
+
+    // O(N): one-dimensional DP array
+    public int _minPathSum(int[][] a) {
+        if (a == null || a.length == 0 || a[0] == null || a[0].length == 0) {
+            return 0;
+        }
+        int nr = a.length;
+        int nc = a[0].length;
+        int[] dp = new int[nc];
+
+        dp[0] = a[0][0];
+        // first row
+        for (int j = 1; j < nc; j++) {
+            dp[j] = dp[j - 1] + a[0][j];
+        }
+
+        for (int i = 1; i < nr; i++) {
+            dp[0] += a[i][0];
+            for (int j = 1; j < nc; j++) {
+                dp[j] = Math.min(dp[j - 1], dp[j]) + a[i][j];
+            }
+        }
+        return dp[nc - 1];
+    }
+
+    public int __minPathSum(int[][] a) {
+
     }
 }
