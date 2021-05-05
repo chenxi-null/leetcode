57faa2fd836581ca0e16c6ba4a7d8cbaf9af57f7
chenxi20 <chenxiCoder@gmail.com>
2021-05-05 23:21:05 +0800

    polish: QuickSort

--- src/main/java/com/leetcode/sorting/QuickSort.java
+++ src/main/java/com/leetcode/sorting/QuickSort.java
@@ -24,8 +24,28 @@ public class QuickSort {
     }
 
     private int partition(int[] a, int start, int end) {
-        int pivotIdx = select(a, start, end);
-        swap(a, pivotIdx, start);
+        int randomIdx = random.nextInt(end - start + 1) + start;
+        swap(a, randomIdx, end);
+
+        int x = a[end];
+        int i = start - 1;
+        for (int j = start; j < end; j++) {
+            if (a[j] < x) {
+                swap(a, ++i, j);
+            }
+        }
+        // a[idx <= i] < x;
+        // a[idx > i] >= x;
+        swap(a, i + 1, end);
+        return i + 1;
+    }
+
+    //----------------------------------------------------
+
+    private int partition_0505(int[] a, int start, int end) {
+        int idx = random.nextInt(end - start + 1) + start;
+        swap(a, idx, start);
+
         int left = start + 1;
         int right = end;
         while (left < right) {
@@ -44,13 +64,11 @@ public class QuickSort {
         return right;
     }
 
+    //----------------------------------------------------
+
     private void swap(int[] a, int i, int j) {
         int temp = a[i];
         a[i] = a[j];
         a[j] = temp;
-    }
-
-    private int select(int[] a, int start, int end) {
-        return random.nextInt(end - start + 1) + start;
     }
 }
