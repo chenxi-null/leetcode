61636c89aa2e7bbcabe95839c21a1e23ca07fcf5
chenxi <chenxiCoder@gmail.com>
2025-03-06 14:03:36 +0800

    #PrefixSum [2588] Count the Number of Beautiful Subarrays

---
+++ src/main/leetcode/editor/cn/2588.count-the-number-of-beautiful-subarrays.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=2588 lang=java
+ *
+ * [2588] Count the Number of Beautiful Subarrays
+ */
+
+// @lc code=start
+class Solution {
+    public long beautifulSubarrays(int[] nums) {
+        Map<Integer, Integer> counter = new HashMap<>();
+        counter.put(0, 1);
+        long ans = 0;
+        int prefix = 0;
+        for (int i = 0; i < nums.length; i++) {
+            prefix ^= nums[i];
+            Integer cnt = counter.get(prefix);
+            if (cnt != null) {
+                ans += cnt;
+            }
+            counter.put(prefix, counter.getOrDefault(prefix, 0) + 1);
+        }
+        return ans;
+    }
+}
+// @lc code=end
